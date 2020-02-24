7b1df13e267a74971d5d74f170eb4e75a6014905
Brittany F <brittfranklin92@gmail.com>
2020-02-24 18:38:21 -0500

    Styling React components

--- react-project/src/Person/Person.js
+++ react-project/src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 // The props argument can be named anything but it is strongly recommended to use props as the name
 
 // props.children will refer to anything that is within the component open and closing tags
 const person = ( props ) => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>I'm {props.name} and I am a {props.age} years old.</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
