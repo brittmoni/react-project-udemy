32041146a2d22f14ba6612d0d90455f9bb3a5636
brittmoni <brittfranklin92@gmail.com>
2020-03-21 19:39:55 -0400

    Outputting conditional content using Javascript

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -45,6 +45,7 @@ class App extends Component {
   }
 
   render () {
+    // Styling a react component
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
@@ -53,28 +54,34 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}/>
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Tony!')}
+            changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}/>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!!</p>
         <button 
           style={style}
-          onClick={this.togglePersonsHandler}>Switch Name</button>
-          {this.state.showPersons ? 
-            <div>
-              <Person 
-                name={this.state.persons[0].name} 
-                age={this.state.persons[0].age}/>
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Tony!')}
-                changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-              <Person 
-                name={this.state.persons[2].name} 
-                age={this.state.persons[2].age}/>
-          </div> : null
-          }
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
       </div>
     );
   }
